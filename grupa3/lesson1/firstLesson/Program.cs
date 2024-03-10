cb2faeb7119e2869ae044ed8b49b9d5a45ae2376
LioBlaja <dan.blajinschi8@gmail.com>
2024-03-10 15:59:03 +0200

    prima lectie

---
+++ grupa3/lesson1/firstLesson/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace firstLesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Console.WriteLine("Hello World!");*/
+            //Console.Write("Hello World!");
+            float test = 5.66F;
+            double test2 = 5.99D;
+            int test3 = 5;
+            bool test4 = true;
+            char test5 = 'a';
+            string test6 = "b";
+        }
+    }
+}
